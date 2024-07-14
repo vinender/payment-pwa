0b7063ba1402859f0b50e6a27f2095854763f1db
vinender <vinendersingh91@gmail.com>
2024-07-14 15:27:27 +0530

    content changed to viewport height

--- src/components/green.js
+++ src/components/green.js
@@ -24,7 +24,7 @@ const MobileSubscriptionScreen = () => {
 
 
   return (
-    <div className="  bg-gray-900 flex justify-center items-center min-h-screen">
+    <div className="  bg-gray-900 flex justify-center items-center h-screen">
       <div className="bg-[#1B9C83] w-full  h-screen overflow-y-auto text-white relative z-20">
         
         <div className="flex items-center justify-evenly space-x-8 pt-8 p-3 ">
@@ -40,53 +40,53 @@ const MobileSubscriptionScreen = () => {
 
         <img alt='flash' src='./demo/flash.png' className='relative -top-2 left-5 z-50 w-[80px] h-[80px]' />
         
-        <div className=" bg-emerald-100 text-gray-800 mx-auto rounded-t-[2rem]  absolute  bottom-0 left-0 right-0 h-[calc(100%-120px)] overflow-y-auto">
-          <div className="bg-white  h-screen p-6  shadow-lg">
+        <div className=" bg-emerald-100 text-gray-800 mx-auto rounded-t-[2rem]  absolute  bottom-0 left-0 right-0 h-[calc(100%-110px)] overflow-y-hidden">
+          <div className="bg-white  h-full p-6    shadow-lg">
             <div className="flex items-center mb-6">
-            <img alt='flash' src='./demo/flash.png' className='relative -top-2 hidden bg-white opacity-90 left-5 z-50 w-[80px] h-[80px]' />
-              <span className="font-semibold relative -top-4 font-poppins left-20 text-xl">One Goal</span>
-            </div>
-
-            <div className='  border-[3.91px]   border-[#1B9C83] rounded-xl'>
-                <div className='rounded-xl bg-emerald-100 p'> 
-                    <button className="bg-emerald-100 border text-black border-green-800  relative top-5 left-5 px-4 py-2 rounded-full border-b-4 text-sm font-bold mb-6">
-                    SUBSCRIBE
-                    </button>
-
-                    {/* <div className="bg-emerald-1200 p-6   mb-6 overflow-x-auto"> */}
-                    <ImageTextSlider slides={slides} />
-                    {/* </div> */}
-            
-
-            </div>
-             
-            <div className="space-y-3 mt-6 p-2 bg-white rounded-xl">
-                <button className="w-full bg-gradient-to-r from-[#1B9C83] to-emerald-400 text-white p-4 rounded-xl text-left flex justify-between items-center">
-                    <div>
-                    <div className="font-poppins font-semibold text-lg">$12 / month</div>
-                    <div className="text-xs font-poppins font-semibold ">Billed $12 every month, starting today</div>
-                    </div>
-                    <div className="w-6 h-6">
-                    <img src='./demo/like.jpeg' className=' ' alt='>' />
-                    </div>
-                </button>
-                
-                <button className="w-full bg-gradient-to-r from-[#1B9C83] to-emerald-400 text-white p-4 rounded-xl text-left flex justify-between items-center">
-                    <div>
-                    <div className="font-poppins font-semibold text-lg">$120 / year</div>
-                    <div className="text-xs font-poppins font-semibold ">Billed $120 every year, starting today</div>
-                    </div>
-                    <div className="w-6 h-6">
-                    <img src='./demo/like.jpeg' className=' ' alt='>' />
-                    </div>
-                </button>
+                <img alt='flash' src='./demo/flash.png' className='relative -top-2 hidden bg-white opacity-90 left-5 z-50 ' />
+                  <span className="font-semibold relative -top-4 font-poppins left-20 text-xl">One Goal</span>
                 </div>
 
+                <div className='  border-[3.91px]  border-[#1B9C83] rounded-xl'>
+                    <div className='rounded-xl  bg-emerald-100 p'> 
+                        <button className="bg-emerald-200 border text-black border-[#1B9C83]  relative top-5 left-5 px-3 py-1 tracking-widest rounded-full border-b-4 text-sm font-bold mb-6">
+                        SUBSCRIBE
+                        </button>
 
-            </div>
-            <p className="text-[13px] w-full h-[40px] font-semibold text-left mt-6 mb-4 text-gray-900 px-6">
-            By continuing, you are indicating that you accept our <span className='underline'>Terms of Service</span>  and <span className='underline'>Privacy Policy</span> .
-          </p>
+                        {/* <div className="bg-emerald-1200 p-6   mb-6 overflow-x-auto"> */}
+                        <ImageTextSlider slides={slides} />
+                        {/* </div> */}
+                
+
+                    </div>
+                
+                    <div className="space-y-3 mt-6 p-2 bg-white rounded-xl">
+                        <button className="w-full bg-gradient-to-r from-[#1B9C83] to-emerald-400 text-white p-4 rounded-xl text-left flex justify-between items-center">
+                            <div>
+                            <div className="font-poppins font-semibold text-md sm:text-lg">$12 / month</div>
+                            <div className="text-xs font-poppins font-semibold ">Billed $12 every month, starting today</div>
+                            </div>
+                            <div className="w-6 h-6">
+                            <img src='./demo/like.jpeg' className=' ' alt='>' />
+                            </div>
+                        </button>
+                        
+                        <button className="w-full bg-gradient-to-r from-[#1B9C83] to-emerald-400 text-white p-4 rounded-xl text-left flex justify-between items-center">
+                            <div>
+                            <div className="font-poppins font-semibold text-md sm:text-lg">$120 / year</div>
+                            <div className="text-xs font-poppins font-semibold ">Billed $120 every year, starting today</div>
+                            </div>
+                            <div className="w-6 h-6">
+                            <img src='./demo/like.jpeg' className=' ' alt='>' />
+                            </div>
+                        </button>
+                    </div>
+
+
+                </div>
+              <p className="text-[13px] w-full h- font-semibold text-left mt-2 sm:mt-6 lg:mt-4 mb-4 text-gray-900 px-1 sm:px-3">
+                By continuing, you are indicating that you accept our <span className='underline'>Terms of Service</span>  and <span className='underline'>Privacy Policy</span> .
+              </p>
           </div>
           
           {/* <p className="text-xs text-center mt-6 mb-4 text-gray-500 px-6">
--- src/components/pager.js
+++ src/components/pager.js
@@ -22,7 +22,7 @@ const ImageTextSlider = ({ slides }) => {
   return (
     <Slider {...settings}>
       {slides.map((slide, index) => (
-        <div key={index} className="text- p-2 h-[300px] relative ">
+        <div key={index} className="text- p-2 h-[250px] sm:h-[230px] md:h-[250px] lg:h-[300px] xl:h-[350px] relative ">
           <img src={slide.image} alt={slide.text} className=" mx-auto  w-[142px] h-[142px]" />
           <p className="text-[18px] h-[60px] p-2  w-full text-gray-900 font-poppins  absolute z-50 -bottom-2 sm:-bottom-2 md:-bottom-5 px-5   bg-white text-left ">{slide.text}</p>
         </div>
